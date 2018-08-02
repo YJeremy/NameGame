3bdba255a6aa53defc27cd276f9622996d767285
jeremyyip <leafjunwing@163.com>
2018-08-02 14:08:51 +0800

    namegame.go v02

--- namegame.go
+++ namegame.go
@@ -14,7 +14,10 @@ import (
 //创建计数器对象
 var wg sync.WaitGroup
 
+//初始化随机数的初始值，否则每次程序开始都是同样的“随机值”
 func init() {
+	//rand.Seed() ，里面加入一个可以变化的参数，让Seed值开始每次都不一样。
+	//time.Now().UnixNano()获取的是当前时间的纳秒，因为计算机运行太快了。这里并不需要连续获取随机数种子，只要每次开机运行代码的时间不同就可以了；
 	rand.Seed(time.Now().UnixNano())
 }
 
@@ -31,30 +34,31 @@ func main() {
 		cli.StringFlag{
 			Name:  "operation,o",
 			Value: "h",
-			Usage: "opertaion",
+			Usage: "opertaion -o p",
 		},
 		cli.StringFlag{
 			Name:  "name1,n1",
-			Value: "",
-			Usage: "user name1",
+			Value: "无名氏1号",
+			Usage: "user name1 -n1 **",
 		},
 		cli.StringFlag{
 			Name:  "name2,n2",
-			Value: "",
-			Usage: "user name2",
+			Value: "无名氏2号",
+			Usage: "user name2:-n2 **",
 		},
 		cli.Int64Flag{
 			Name:  "name1HP,hp1",
 			Value: 20,
-			Usage: "user name1 HP",
+			Usage: "user name1 HP:-hp1 20",
 		},
 		cli.Int64Flag{
 			Name:  "name2HP,hp2",
 			Value: 20,
-			Usage: "user name2 HP",
+			Usage: "user name2 HP:-hp2 20",
 		},
 	}
 
+	//重写app.Action的方法
 	app.Action = func(c *cli.Context) error {
 
 		operation := c.String("operation")
@@ -67,8 +71,8 @@ func main() {
 		wg.Add(2)
 
 		if operation == "p" {
-			go player(name1, court, 1, name1hp)
-			go player(name2, court, 1, name2hp)
+			go player(name1, court, 1, name2hp)
+			go player(name2, court, 1, name1hp)
 
 			// 给通道传入数据，触发并发开始游戏
 			court <- 1
@@ -78,6 +82,8 @@ func main() {
 		}
 		return nil
 	}
+
+	//获取参数，app运行
 	app.Run(os.Args)
 
 }
@@ -99,17 +105,8 @@ func player(name string, court chan int, turn, hp int) {
 			return
 		}
 
-		n := rand.Intn(10)
+		n := rand.Intn(10) //产生给对方伤害值
 		hecainum := rand.Intn(4)
-
-		if hp < 0 {
-			hp = 0
-			fmt.Printf("KO！%s将对方血量打到0！ %s胜利！\n", name, name)
-
-			// 关闭通道，退出循环
-			close(court)
-			return
-		}
 
 		fmt.Printf("第%d回合 ", turn)
 		turn++
@@ -118,11 +115,19 @@ func player(name string, court chan int, turn, hp int) {
 			fmt.Printf("你的名字|%-6s|使出秘籍“要你命3000”！直接将对手血量打成0！\n%s胜利！\n", name, name)
 			close(court)
 			return
-		} else {
-
-			hp = hp - n
-			fmt.Printf("你的名字|%-6s|使用|%s输出%d点伤害,%-6s|\t对手血量%d\n", name, skill[n], n, hecai[hecainum], hp)
 		}
+
+		hp = hp - n //血量减去伤害
+		if hp <= 0 {
+			hp = 0
+			fmt.Printf("KO！%s将对方血量打到0！ %s胜利！\n", name, name)
+
+			// 关闭通道，退出循环
+			close(court)
+			return
+		}
+		fmt.Printf("你的名字|%-6s|使用|%s输出%d点伤害,%-6s|\t将对手血量降到%d\n", name, skill[n], n, hecai[hecainum], hp)
+
 		time.Sleep(1 * time.Second)
 		court <- battle //阻塞通道
 	}
